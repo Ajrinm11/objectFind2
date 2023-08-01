5239f5310b278cb1775eedd664503a5978c58cd4
Ajrinm11 <84498693+Ajrinm11@users.noreply.github.com>
2023-08-01 12:22:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+video="";
+Status="";
+
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(480, 380);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+image(video, 0,0, 480, 380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model loaded!");
+    Status=true;
+    video.loop();
+    video.volume(0);
+    video.speed(1);
+}
